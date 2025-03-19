1b862f645545af9baae83cd7e6bec6945e1b2cd2
jcrippen01 <justin.crippen@gmail.com>
2025-03-18 20:11:42 -0500

    restructering backend

--- mindfulness_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MindfulnessAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mindfulness_app'
--- trails_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrailsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trails_app'
