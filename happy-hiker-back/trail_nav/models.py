f918b989102a70f81fe6b32a476af501dec0750b
jcrippen01 <justin.crippen@gmail.com>
2025-03-18 21:26:04 -0500

    started backend

--- happy-hiker-back/trail_nav/models.py
+++ happy-hiker-back/trail_nav/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Trail(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="trail_images/", null=True, blank=True)
+    lat = models.FloatField()
+    long = models.FloatField()
+    distance = models.FloatField(help_text="Trail distance in miles")
+    elevation = models.FloatField(help_text="Elevation gain in feet")
+    difficulty = models.CharField(
+        max_length=50,
+        choices=[("Easy", "Easy"), ("Moderate", "Moderate"), ("Hard", "Hard")],
+    )
+    is_dog_friendly = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
