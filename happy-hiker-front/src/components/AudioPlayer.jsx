3c5b8377a73f13f0ab773c19f90fe082b174bd69
Jonathan-Taylor319 <jontaylor319@gmail.com>
2025-03-20 18:43:07 -0400

    daily upload

--- happy-hiker-front/src/components/AudioPlayer.jsx
+++ happy-hiker-front/src/components/AudioPlayer.jsx
@@ -3,15 +3,32 @@ import ReactHowler from "react-howler";
 
 const AudioPlayer = () => {
   const [playing, setPlaying] = useState(false);
+  // need to make a state to control volume
+  const [volume, setVolume] = useState(50)
 
  
 
   return (
-    <div>
-      <ReactHowler src="https://happyhiker-audio.s3.us-east-2.amazonaws.com/file_example_MP3_700KB.mp3" playing={playing} />
+    <div className="Audio-Player">
+      <ReactHowler src="https://happyhiker-audio.s3.us-east-2.amazonaws.com/file_example_MP3_700KB.mp3" 
+        playing={playing}
+        volume = { volume / 100 }
+        initialVolume = {50}
+      />
       <button onClick={() => setPlaying(!playing)}>
         {playing ? "Pause" : "Play"}
       </button>
+      <div>
+        <label for="slider">Volume</label>
+        <input
+          type = "range"
+          min = "0"
+          max = "100"
+          value = { volume }
+          className = "slider"
+          onChange={(e) => setVolume(Number(e.target.value))}
+        />
+      </div>
     </div>
   );
 };
---
+++ happy-hiker-front/src/components/MessageSettings.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function MessageButtons() {
+    const [buttonOn, setButtonOn] = useState(false)
+
+    return(
+        <>
+            <button> 5 minute delay </button>
+            
+        </>
+    )
+}
